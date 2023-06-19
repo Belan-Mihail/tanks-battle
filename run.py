d64c00fd3debe58fadedbf5ecd2ff9ef6dfafcb5
Belan-Mihail Belan <belan_mihail@inbox.ru>
2023-06-19 20:59:10 +0000

    Writing a function to select the complexity of the game

--- run.py
+++ run.py
@@ -6,7 +6,8 @@ def introduction_func():
     Displays a welcome message to the user with a description of the game,
     and also asks for a username
     """
-    introduction = """Welcome to the tanks battle game!
+    introduction = """
+    Welcome to the tanks battle game!
     In this game, the one who quickly knocks out all the enemy tanks will win.
     Intrigued? Then let's get started...\n"""
 
@@ -17,6 +18,14 @@ def introduction_func():
 
 
 user_name = introduction_func()
+
+
+def game_difficult_selection():
+    """
+    Asks the user for input data on the basis of which the complexity
+    of the game requested by the user is calculated
+    """
+    print(f'Hi {user_name}! First... choose the difficulty of the game')
 
 
 
