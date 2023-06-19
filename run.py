ca7ac28e56f76ae31cd874049dc547a5b1489a7e
Belan-Mihail Belan <belan_mihail@inbox.ru>
2023-06-19 21:08:05 +0000

    Finish a function to select the complexity of the game

--- run.py
+++ run.py
@@ -20,12 +20,26 @@ def introduction_func():
 user_name = introduction_func()
 
 
-def game_difficult_selection():
+def game_difficulty_selection():
     """
     Asks the user for input data on the basis of which the complexity
     of the game requested by the user is calculated
     """
+    print()
     print(f'Hi {user_name}! First... choose the difficulty of the game')
+    time.sleep(2)
+    print('Enter a number from 1 to 3, where 3 is the highest difficulty\n')
+    choice = input()
+
+    while choice != '1' and choice != '2' and choice != '3':
+        print('Incorrect data entered. Enter a number from 1 to 3')
+        choice = input()
+    return choice
+
+
+user_choice = game_difficulty_selection()
+
+
 
 
 
