531b274b1d8c48d06969f54753472ec80b0396d0
Belan-Mihail Belan <belan_mihail@inbox.ru>
2023-06-19 20:49:41 +0000

    Add variable with username and clear code

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 import time
 
+
 def introduction_func():
     """
-    Displays a welcome message to the user with a description of the game, 
+    Displays a welcome message to the user with a description of the game,
     and also asks for a username
     """
-    introduction = """Welcome to the tanks battle game! 
-    In this game, the one who quickly knocks out all the enemy tanks will win. 
+    introduction = """Welcome to the tanks battle game!
+    In this game, the one who quickly knocks out all the enemy tanks will win.
     Intrigued? Then let's get started...\n"""
 
     print(introduction)
@@ -15,7 +16,7 @@ def introduction_func():
     return user
 
 
-
+user_name = introduction_func()
 
 
 
